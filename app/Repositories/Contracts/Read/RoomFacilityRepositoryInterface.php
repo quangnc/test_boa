be01dca4bec48b08a5b03ae760742b759cbd8fab
quangnc <chiquang.dev@gmail.com>
2019-07-14 02:49:01 +0700

    remove file

--- app/Repositories/Contracts/Read/RoomFacilityRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Repositories\Contracts\Read;
-interface RoomFacilityRepositoryInterface
-{
-    
-}
--- app/Repositories/Contracts/Write/RoomFacilityRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Repositories\Contracts\Write;
-interface RoomFacilityRepositoryInterface
-{
-    
-}
